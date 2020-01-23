63de141ac90f829d1b04a1cf982f7ad01cf73e36
kamleshchandnani <kamlesh.chandnani@gmail.com>
2020-01-23 22:30:29 +0530

    fix(tree-shaking): add babel plugin for tree shaking

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,14 @@ module.exports = function babelConfig(api) {
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
     plugins: [
+      [
+        'babel-plugin-styled-components',
+        {
+          displayName: true,
+          pure: true,
+          ssr: true
+        }
+      ],
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-export-default-from',
       '@babel/plugin-proposal-export-namespace-from',
